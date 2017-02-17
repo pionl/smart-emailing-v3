9927098ed58ac812f2012c8ae5f4f7d07cd99446
Martin Kluska <martin.kluska@imakers.cz>
2017-02-17 15:16:22 +0100

    Added new exception to raise error when property is not set before sending request

---
+++ src/Exceptions/PropertyRequiredException.php
@@ -0,0 +1,27 @@
+<?php
+namespace SmartEmailing\v3\Exceptions;
+
+use Exception;
+
+class PropertyRequiredException extends \LogicException
+{
+    /**
+     * Throws PropertyRequiredException if $condition is false
+     * @param string $propertyName
+     * @param boolean $condition
+     *
+     * @throws PropertyRequiredException
+     */
+    public static function throwIf($propertyName, $condition)
+    {
+        if (!$condition) {
+            throw new PropertyRequiredException($propertyName);
+        }
+    }
+
+    public function __construct($propertyName, $code = 500, \Exception $previous = null)
+    {
+        parent::__construct("Property {$propertyName} is required to be set", $code, $previous);
+    }
+
+}
